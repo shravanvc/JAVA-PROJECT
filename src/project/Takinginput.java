340b6003c7aed0667bcd84df9067989f9d315896
shravan <shravankumarc87@gmail.com>
2025-02-20 23:12:15 +0530

    init commit

--- src/project/Takinginput.java
+++ src/project/Takinginput.java
@@ -1,4 +1,22 @@
 package project;
+import java.util.Scanner;
 
 public class Takinginput {
+    public static void main(String[] args) {
+        System.out.println("taking input from user ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+   //     int a = sc.nextInt();
+        float a = sc.nextFloat();
+    //    boolean b1 = sc.hasNextFloat();
+        //   System.out.println(b1);
+        System.out.println("Enter number 2");
+        float b = sc.nextFloat();
+    //    int b = sc.nextInt();
+    //    boolean b2 = sc.hasNextFloat();
+    //    System.out.println(b2);
+        float sum = a + b;
+        System.out.println("the sum of these number is:");
+        System.out.println(sum);
+    }
 }
--- src/project/literals.java
+++ src/project/literals.java
@@ -1,4 +1,17 @@
 package project;
 
 public class literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age1 = 39;
+        short age2 = 23;
+        long age4 = 35445653876898L;
+        char ch = 'A';
+        float f1 = 6.7f;
+        double d1 = 4.773;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Harry";
+        System.out.println(str);
+    }
 }
