3462ecc67d72fede9f0d5672d0a51548438a6cca
shravan <shravankumarc87@gmail.com>
2025-02-20 23:42:52 +0530

    first commit

--- src/project/SumExample.java
+++ src/project/SumExample.java
@@ -1,16 +1,16 @@
-package project;
-    public class SumExample {
-        public static void main(String[] args) {
-            int sum = 0;
-
-            // Loop through numbers 1 to 10
-            for (int i = 1; i <= 10; i++) {
-                sum += i;  // Add the current number to sum
-            }
-
-            // Print the sum
-            System.out.println("The sum of numbers from 1 to 10 is: " + sum);
-        }
-    }
-
-
+//package project;
+//    public class SumExample {
+//        public static void main(String[] args) {
+//            int sum = 0;
+//
+//            // Loop through numbers 1 to 10
+//            for (int i = 1; i <= 10; i++) {
+//                sum += i;  // Add the current number to sum
+//            }
+//
+//            // Print the sum
+//            System.out.println("The sum of numbers from 1 to 10 is: " + sum);
+//        }
+//    }
+//
+//
