8089850dd0c3a508aea7d82bb2e4843caa46d9b5
shravan <shravankumarc87@gmail.com>
2025-02-23 20:07:07 +0530

    first commit

--- src/project/calculator.java
+++ src/project/calculator.java
@@ -1,63 +1,63 @@
-package project;
-import java.util.Scanner;
-
-public class calculator {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Calculator!");
-
-        while (true) {
-            System.out.print("Enter first number: ");
-            double num1 = scanner.nextDouble();
-
-            System.out.print("Enter an operator (+, -, *, /): ");
-            char operator = scanner.next().charAt(0);
-
-            System.out.print("Enter second number: ");
-            double num2 = scanner.nextDouble();
-
-            double result = 0;
-            boolean validOperator = true;
-
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 != 0) {
-                        result = num1 / num2;
-                    } else {
-                        System.out.println("Error: Division by zero is not allowed.");
-                        validOperator = false;
-                    }
-                    break;
-                default:
-                    System.out.println("Error: Invalid operator.");
-                    validOperator = false;
-                    break;
-            }
-
-            if (validOperator) {
-                System.out.println("The result is: " + result);
-            }
-
-            System.out.print("Would you like to perform another calculation? (yes/no): ");
-            String choice = scanner.next();
-
-            if (!choice.equalsIgnoreCase("yes")) {
-                break;
-            }
-        }
-
-        System.out.println("Thank you for using the calculator. Goodbye!");
-        scanner.close();
-    }
-}
+//package project;
+//import java.util.Scanner;
+//
+//public class calculator {
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Welcome to the Calculator!");
+//
+//        while (true) {
+//            System.out.print("Enter first number: ");
+//            double num1 = scanner.nextDouble();
+//
+//            System.out.print("Enter an operator (+, -, *, /): ");
+//            char operator = scanner.next().charAt(0);
+//
+//            System.out.print("Enter second number: ");
+//            double num2 = scanner.nextDouble();
+//
+//            double result = 0;
+//            boolean validOperator = true;
+//
+//            switch (operator) {
+//                case '+':
+//                    result = num1 + num2;
+//                    break;
+//                case '-':
+//                    result = num1 - num2;
+//                    break;
+//                case '*':
+//                    result = num1 * num2;
+//                    break;
+//                case '/':
+//                    if (num2 != 0) {
+//                        result = num1 / num2;
+//                    } else {
+//                        System.out.println("Error: Division by zero is not allowed.");
+//                        validOperator = false;
+//                    }
+//                    break;
+//                default:
+//                    System.out.println("Error: Invalid operator.");
+//                    validOperator = false;
+//                    break;
+//            }
+//
+//            if (validOperator) {
+//                System.out.println("The result is: " + result);
+//            }
+//
+//            System.out.print("Would you like to perform another calculation? (yes/no): ");
+//            String choice = scanner.next();
+//
+//            if (!choice.equalsIgnoreCase("yes")) {
+//                break;
+//            }
+//        }
+//
+//        System.out.println("Thank you for using the calculator. Goodbye!");
+//        scanner.close();
+//    }
+//}
